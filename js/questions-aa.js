cff442b6f2c09c53a57dee9286840e8b892c078a
Mateusz Tomasiak <mateusz.tomasiak02@gmail.com>
2026-02-01 16:23:37 +0100

    Add AA, TS, SW questions

---
+++ js/questions-aa.js
@@ -0,0 +1,358 @@
+window.questionsAA = [
+  {
+    "question": "Zaznaczyć, jakie funkcje spełnia przetwornik pomiarowy w układzie regulacji automatycznej rzeczywistego procesu przemysłowego",
+    "options": [
+      "wzmocnienie sygnału sterującego",
+      "wzmocnienie sygnału z czujnika pomiarowego",
+      "stworzenie warunków do unifikacji aparatury automatyki w zakresie dopasowania wejść i wyjść",
+      "nie spełnia żadnej z powyższych funkcji"
+    ],
+    "answer": [
+      "wzmocnienie sygnału z czujnika pomiarowego",
+      "stworzenie warunków do unifikacji aparatury automatyki w zakresie dopasowania wejść i wyjść"
+    ]
+  },
+  {
+    "question": "W których z wymienionych poniżej sytuacji do pomiaru temperatury może być zastosowany czujnik temperatury Pt-100?",
+    "options": [
+      "w zakresie niskich i średnich temperatur do około 600°C",
+      "w zakresie bardzo wysokich temperatur powyżej 1000°C",
+      "w środowisku silnie korozyjnym",
+      "w środowisku wybuchowym"
+    ],
+    "answer": [
+      "w zakresie niskich i średnich temperatur do około 600°C"
+    ]
+  },
+  {
+    "question": "Do pomiaru natężenia przepływu których z wymienionych poniżej cieczy może być stosowanyprzepływomierz indukcyjny?",
+    "options": [
+      "benzyna",
+      "kwas siarkowy",
+      "olej roślinny",
+      "sok pomarańczowy"
+    ],
+    "answer": [
+      "kwas siarkowy",
+      "sok pomarańczowy"
+    ]
+  },
+  {
+    "question": "Które z wymienionych poniżej zakładów lub linii technologicznych charakteryzują się podwyższonym zagrożeniem wybuchowym i wymagają konstrukcji systemu automatyki z zachowaniem wymogów iskrobezpieczeństwa?",
+    "options": [
+      "stacja redukcyjno – pomiarowa gazu ziemnego",
+      "tartak",
+      "linia pakowania sproszkowanych zapraw murarskich",
+      "lakiernia"
+    ],
+    "answer": [
+      "stacja redukcyjno – pomiarowa gazu ziemnego",
+      "tartak",
+      "lakiernia"
+    ]
+  },
+  {
+    "question": "Zaznaczyć cechy przemysłowej sieci komputerowej.",
+    "options": [
+      "determinizmn czasowy",
+      "bezpieczeństwo wysyłania dużych pakietów danych",
+      "możliwość zasilania pewnych urządzeń z magistrali",
+      "możliwość przeglądania stron internetowych"
+    ],
+    "answer": [
+      "determinizmn czasowy",
+      "możliwość zasilania pewnych urządzeń z magistrali"
+    ]
+  },
+  {
+    "question": "Które z podanych warstw modelu OSI są zdefiniowane dla sieci przemysłowej PROFIBUS?",
+    "options": [
+      "warstwa fizyczna",
+      "warstwa sieciowa",
+      "warstwa sesji",
+      "warstwa łącza danych (liniowa)"
+    ],
+    "answer": [
+      "warstwa fizyczna",
+      "warstwa łącza danych (liniowa)"
+    ]
+  },
+  {
+    "question": "Jaki eksperyment należy przeprowadzić w celu dostrojenia regulatora PID metodą cyklu granicznego?",
+    "options": [
+      "eksperymentalne wyznaczenie stałej czasowej i czasu opóźnienia obiektu regulacji ",
+      "eksperymentalne wyznaczenie wzmocnienia statycznego obiektu regulacji",
+      "eksperymentalne wyznaczenie wzmocnienia krytycznego i okresu oscylacji nietłumionych w układzie regulacji",
+      "eksperymentalne wyznaczenie charakterystyki statycznej obiektu regulacji"
+    ],
+    "answer": [
+      "eksperymentalne wyznaczenie wzmocnienia krytycznego i okresu oscylacji nietłumionych w układzie regulacji"
+    ]
+  },
+  {
+    "question": "Które z wymienionych poniżej czynników mogą znacząco wydłużyć wartość czasu odpowiedzi sterownika PLC?",
+    "options": [
+      "operacje obliczeniowe i porównania wykonywane na danych typu REAL",
+      "operacje logiczne na bitach",
+      "operacje na danych całkowitych (typu INT, DINT)",
+      "obliczenie funkcji trygonometrycznych"
+    ],
+    "answer": [
+      "operacje obliczeniowe i porównania wykonywane na danych typu REAL",
+      "obliczenie funkcji trygonometrycznych"
+    ]
+  },
+  {
+    "question": "Zaznaczyć podstawowe cechy użytkowe siłownika pneumatycznego tłokowego",
+    "options": [
+      "pełne iskrobezpieczeństwo",
+      "możliwość uzyskania dużych sił z elementu o niewielkich gabarytach",
+      "położenie organu ruchomego pozostaje bez zmian po zaniku zasilania",
+      "przeciążalność aż do zatrzymania nie grożąca uszkodzeniem elementu"
+    ],
+    "answer": [
+      "pełne iskrobezpieczeństwo",
+      "przeciążalność aż do zatrzymania nie grożąca uszkodzeniem elementu"
+    ]
+  },
+  {
+    "question": "Ustawnik pozycyjny (pozycjoner) stosowany jest w układach siłowników pneumatycznych w celu:",
+    "options": [
+      "zwiększenia mocy na wyjściu siłownika",
+      "zwiększenia dokładności pozycjonowania organu ruchomego siłownika",
+      "zapewnienia iskorobezpieczeństwa siłownika",
+      "łatwego podłączenia siłownika do sieci przemysłowej (ustawnik z wejściem cyfrowym)"
+    ],
+    "answer": [
+      "zwiększenia dokładności pozycjonowania organu ruchomego siłownika"
+    ]
+  },
+  {
+    "question": "Zaznaczyć, jakich podstawowych założeń należy przestrzegać podczas konstrukcji obwodów zapewniających bezpieczeństwo obsługi procesu?",
+    "options": [
+      "Powinny być zrealizowane z użyciem elementów łatwych do naprawy przez służby utrzymania ruchu w zakładzie",
+      "Powinny być skonstruowane przy założeniu wystąpienia najbardziej niekorzystnej sytuacji podczas pracy systemu",
+      "Powinny być skonstruowane z użyciem najprostszych elementów o jak najwyższej niezawodności (np. Wyłączniki termiczne i ciśnieniowe)",
+      "Obwody zabezpieczające powinny być częścią obwodu sterowania procesem (np. Być zrealizowane na tym samym sterowniku PLC)"
+    ],
+    "answer": [
+      "Powinny być skonstruowane przy założeniu wystąpienia najbardziej niekorzystnej sytuacji podczas pracy systemu",
+      "Powinny być skonstruowane z użyciem najprostszych elementów o jak najwyższej niezawodności (np. Wyłączniki termiczne i ciśnieniowe)"
+    ]
+  },
+  {
+    "question": "Jakie jest maksymalne obciążenie rezystancyjne w pętli prądowej w obwodzie wyjściowym przetwornika analogowego z wyjściem prądowym 4 – 20 [mA]?",
+    "options": [
+      "1000 Ohm",
+      "500 Ohm",
+      "750 Ohm",
+      "100 Ohm"
+    ],
+    "answer": [
+      "500 Ohm",
+    ]
+  },
+  {
+    "question": "Które z podanych poniżej warstw modelu OSI są zdefiniowane dla sieci przemysłowej HART?",
+    "options": [
+      "warstwa fizyczna",
+      "warstwa aplikacyjna",
+      "warstwa sesji",
+      "warstwa łącza danych (liniowa)"
+    ],
+    "answer": [
+      "warstwa fizyczna",
+      "warstwa aplikacyjna",
+      "warstwa łącza danych (liniowa)"
+    ]
+  },
+  {
+    "question": "Które z wymienionych poniżej algorytmów regulacyjnych mogą być realizowane przez regulator bezpośredniego działania? ",
+    "options": [
+      "dwupołożeniowy",
+      "PI",
+      "P",
+      "PID"
+    ],
+    "answer": [
+      "dwupołożeniowy",
+      "P"
+    ]
+  },
+  {
+    "question": "W której części rurociągu powinien być zamontowany czujnik do pomiaru ciśnienia gazu?",
+    "options": [
+      "W górnej części",
+      "W prostym odcinku rurociągu",
+      "W dolnej części",
+      "Nie ma znaczenia"
+    ],
+    "answer": [
+      "W górnej części",
+    ]
+  },
+  {
+    "question": "Zaznaczyć cechy użytkowe siłownika elektrycznego",
+    "options": [
+      "przeciążalność organu ruchomego bez ryzyka uszkodzenia",
+      "w przypadku zaniku zasilania organ ruchomy pozostaje w ostatnim położeniu",
+      "łatwa realizacja pomocniczego sterowania ręcznego",
+      "pełne iskrobezpieczeństwo"
+    ],
+    "answer": [
+      "w przypadku zaniku zasilania organ ruchomy pozostaje w ostatnim położeniu",
+      "łatwa realizacja pomocniczego sterowania ręcznego",
+    ]
+  },
+  {
+    "question": "Czujnik pomiarowy poprawnie dobrany do sterowanego procesu powinien spełniać następujące warunki",
+    "options": [
+      "Dynamikę porównywalną z dynamiką sterowanego procesu",
+      "Żadna z wymienionych cech nie ma znaczenia",
+      "Zakres pomiarowy nieco szerszy, niż przewidywany zakres zmienności wielkości regulowanej",
+      "Obudowę i przyłącze odporne na warunki pracy"
+    ],
+    "answer": [
+      "Zakres pomiarowy nieco szerszy, niż przewidywany zakres zmienności wielkości regulowanej",
+      "Obudowę i przyłącze odporne na warunki pracy"
+    ]
+  },
+  {
+    "question": "W obwodzie zabezpieczeniowym silnika elektrycznego przed przegrzaniem należy zastosować",
+    "options": [
+      "Piezorezystor",
+      "Wyłącznik termiczny",
+      "Czujnik Pt 100",
+      "Termoparę"
+    ],
+    "answer": [
+      "Wyłącznik termiczny"
+    ]
+  },
+  {
+    "question": "Termoparę z przetwornikiem należy łączyć za pomocą",
+    "options": [
+      "Trzema jednakowymi przewodami",
+      "Rodzaj przewodów nie ma znaczenia dla poprawności działania",
+      "Przewodami kompensacyjnymi",
+      "Czterema jednakowymi przewodami"
+    ],
+    "answer": [
+      "Przewodami kompensacyjnymi"
+    ]
+  },
+  {
+    "question": "Czynnikiem zakłócającym pomiar ciśnienia czujnikiem piezorezystancyjnym jest",
+    "options": [
+      "Zużywanie się czujnika wraz z upływem czasu",
+      "Zmienna gęstość mierzonego medium",
+      "Zmienne pH mierzonego medium",
+      "Zmienna temperatura mierzonego medium"
+    ],
+    "answer": [
+      "Zużywanie się czujnika wraz z upływem czasu",
+      "Zmienna temperatura mierzonego medium"
+    ]
+  },
+  {
+    "question": "Zaznaczyć podstawowe cechy przepływomierza zwężkowego",
+    "options": [
+      "Liniowa charaktersytyka przetwarzania",
+      "Możliwość pomiaru przepływu zawiesin i mieszanin",
+      "Zależność dokładności pomiaru od temperatury medium",
+      "Straty energii przepływającej cieczy podczas pomiaru"
+    ],
+    "answer": [
+      "Zależność dokładności pomiaru od temperatury medium",
+      "Straty energii przepływającej cieczy podczas pomiaru"
+    ]
+  },
+  {
+    "question": "W której części rurociągu lub zbiornika należy zamontować czujnik do pomiaru ciśnienia cieczy?",
+    "options": [
+      "W dolnej części",
+      "W prostym odcinku rurociągu",
+      "W górnej części",
+      "Nie ma znaczenia"
+    ],
+    "answer": [
+      "W dolnej części"
+    ]
+  },
+  {
+    "question": "W której części rurociągu należy zamontować przepływomierz?",
+    "options": [
+      "W najniższej części rurociągu",
+      "Blisko swobodnego wypływu z rurociągu do kadzi lub rynny",
+      "W najwyższej części rurociągu",
+      "Nie ma znaczenia"
+    ],
+    "answer": [
+      "W najniższej części rurociągu",
+    ]
+  },
+  {
+    "question": "Co jest potrzebne do instalacji obudowy przewietrzanej?",
+    "options": [
+      "Instalacja elektryczna 400V",
+      "Instalacja sprężonego powietrza",
+      "Instalacja wodociągowa",
+      "Wentylator"
+    ],
+    "answer": [
+      "Instalacja sprężonego powietrza"
+    ]
+  },
+  {
+    "question": "Częściami sumatora pneumatycznego są:",
+    "options": [
+      "Układ dysza – przesłona",
+      "Elektromagnes",
+      "Mieszek sprężysty",
+      "Bezpiecznik topikowy"
+    ],
+    "answer": [
+      "Układ dysza – przesłona",
+      "Mieszek sprężysty"
+    ]
+  },
+  {
+    "question": "W których z podanych poniżej systemach sterowania, monitorowania i nadzoru można wykorzystać sieć telefonii komórkowej?",
+    "options": [
+      "Sterowanie robotami przemysłowymi na linii produkcyjnej",
+      "Zdalny nadzór bezobsługowej oczyszczalni ścieków",
+      "System automatyki domowej",
+      "Koordynacja pracy maszyn na linii montażowej"
+    ],
+    "answer": [
+      "Zdalny nadzór bezobsługowej oczyszczalni ścieków",
+      "System automatyki domowej"
+    ]
+  },
+  {
+    "question": "Zaznacz cechy funkcjonalne siłownika hydraulicznego.",
+    "options": [
+      "Iskrobezpieczeństwo pod warunkiem zastosowania niepalnego oleju w systemie zasilania",
+      "Duża moc przy niewielkich wymiarach",
+      "Proste do uzyskania precyzje pozycjonowanie organu ruchomego",
+      "Złożona konstrukcja układu zasilania i sterowania"
+    ],
+    "answer": [
+      "Iskrobezpieczeństwo pod warunkiem zastosowania niepalnego oleju w systemie zasilania",
+      "Duża moc przy niewielkich wymiarach",
+      "Złożona konstrukcja układu zasilania i sterowania"
+    ]
+  },
+  {
+    "question": "W sieci PROFINET najwyższy priorytet ma kanał:",
+    "options": [
+      "RT",
+      "IRT",
+      "NRT",
+      "TCP/IP"
+    ],
+    "answer": [
+      "IRT"
+    ]
+  },
+];
---
+++ js/questions-wizyjne.js
@@ -0,0 +1,141 @@
+window.questionsWizyjne = [
+  {
+    "question": "Gdzie znajdują się elementy światłoczułe w oku człowieka?",
+    "options": [
+      "W rogówce",
+      "W siatkówce",
+      "W soczewce",
+      "W źrenicy"
+    ],
+    "answer": [
+      "W siatkówce"
+    ]
+  },
+  {
+    "question": "Jakie elementy światłoczułe w oku człowieka zapewniają widzenie barw (kolorów)?",
+    "options": [
+      "Pręciki",
+      "Czopki",
+      "Komórki Daltona",
+      "Retina"
+    ],
+    "answer": [
+      "Czopki"
+    ]
+  },
+  {
+    "question": "Który z poniższych skrótów jest oznaczeniem modelu przestrzeni barw (kolorów)?",
+    "options": [
+      "HSV",
+      "ELO",
+      "CHGW",
+      "BGR"
+    ],
+    "answer": [
+      "HSV",
+      "BGR"
+    ]
+  },
+  {
+    "question": "Ilu najbliższych sąsiadów ma punkt obrazu cyfrowego o rastrze prostokątnym (pomijając punkty brzegowe)?",
+    "options": [
+      "2",
+      "4",
+      "8",
+      "12"
+    ],
+    "answer": [
+      "4",
+      "8"
+    ]
+  },
+  {
+    "question": "Jaką operacją jest przekształcenie obrazu za pomocą LUT (tablicy przekodowań, Look-Up Table)?",
+    "options": [
+      "punktową",
+      "odwracalną",
+      "kontekstową",
+      "bezkontekstową"
+    ],
+    "answer": [
+      "punktową",
+      "bezkontekstową"
+    ]
+  },
+  {
+    "question": "Jakim przekształceniem jest filtracja medianowa?",
+    "options": [
+      "liniowym",
+      "nieliniowym",
+      "liniowym lub nieliniowym - zależy to od postaci filtru",
+      "częściowo liniowym - zależy to od postaci flitru i zawartości filtrowanego obrazu"
+    ],
+    "answer": [
+      "nieliniowym"
+    ]
+  },
+  {
+    "question": "X i Y to wyniki konwolucji odpowiednio z pionową i poziomą maską gradientu Sobela. Jaka formuła jest stosowana przy wyliczaniu końcowego rezultatu?",
+    "options": [
+      "L' = |X| + |Y|",
+      "L' = X² + Y²",
+      "L' = X + Y",
+      "L' = √(X² + Y²)"
+    ],
+    "answer": [
+      "L' = |X| + |Y|",
+      "L' = √(X² + Y²)"
+    ]
+  },
+  {
+    "question": "Czy binaryzacja i progowanie (thresholding) to dwie różne nazwy tej samej metody segmentacji obrazu?",
+    "options": [
+      "Tak, to są synonimy",
+      "Nie, binaryzacja to szczególny przypadek progowania",
+      "Nie, progowanie to szczególny przypadek binaryzacji",
+      "Nie, to zupełnie różne metody"
+    ],
+    "answer": [
+      "Nie, binaryzacja to szczególny przypadek progowania"
+    ]
+  },
+  {
+    "question": "Od jakich cech obiektu nie powinny zależeć wartości współczynników kształtu?",
+    "options": [
+      "kształtu",
+      "wielkości",
+      "położenia",
+      "orientacji"
+    ],
+    "answer": [
+      "wielkości",
+      "położenia",
+      "orientacji"
+    ]
+  },
+  {
+    "question": "Jaki typ obrazu powinien stanowić dane wejściowe dla transformaty Hougha?",
+    "options": [
+      "Obraz binarny z pełną informacją o obiektach",
+      "Obraz binarny z częściową informacją o obiektach",
+      "Obraz z poziomami szarości",
+      "Obraz barwny (kolorowy)"
+    ],
+    "answer": [
+      "Obraz binarny z częściową informacją o obiektach"
+    ]
+  },
+  {
+    "question": "Która z poniższych binarnych operacji morfologicznych może doprowadzić do zmniejszenia obiektu?",
+    "options": [
+      "erozja (erosion)",
+      "dylatacja (dilation)",
+      "otwarcie (opening)",
+      "domknięcie/zamknięcie (closing)"
+    ],
+    "answer": [
+      "erozja (erosion)",
+      "otwarcie (opening)"
+    ]
+  },
+];
