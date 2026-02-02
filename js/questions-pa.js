d68829be27ae2ad6dfdc63dec8d7218e0acb36f9
Mateusz Tomasiak <mateusz.tomasiak02@gmail.com>
2026-02-02 10:40:12 +0100

    Add PA questions

---
+++ js/questions-pa.js
@@ -0,0 +1,269 @@
+window.questionsPA = [
+  {
+    "question": "Na podstawie przebiegu charakterystyki statycznej obiektu regulacji można określić:",
+    "options": [
+      "lokalizację zer i biegunów transmitancji obiektu",
+      "zakres, w którym obiekt może być uznany za liniowy",
+      "wzmocnienie statyczne obiektu w punkcie pracy",
+      "wielkość opóźnienia obiektu",
+    ],
+    "answer": [
+      "zakres, w którym obiekt może być uznany za liniowy",
+      "wzmocnienie statyczne obiektu w punkcie pracy"
+    ]
+  },
+  {
+    "question": "Dany jest wzór opisujący analityczną postać odpowiedzi czasowej obiektu: 𝒚(𝒕) = 𝑳−𝟏{𝑼(𝒔) ∗ 𝑮(𝒔)} wzór ten opisuje:",
+    "options": [
+      "loraz transformaty Laplace’a Y(s) funkcji wyjścia systemu y(t) otrzymanego jako odpowiedź na sterowanie u(t), do transformaty Laplace’a tego sterowania U(s) - przy zerowych warunkach początkowych",
+      "iloraz transformaty Laplace’a funkcji wyjścia systemu Y(s) otrzymanego jako odpowiedź na sterowanie u(t), do transformaty Laplace’a tego sterowania U(s)",
+      "iloraz transformaty Laplace’a funkcji wejścia systemu U(s), do transformaty Laplace’a wyjścia Y(s)",
+      "iloraz funkcji wyjścia systemu do sterowania - przy zerowych warunkach początkowych",
+    ],
+    "answer": [
+      "loraz transformaty Laplace’a Y(s) funkcji wyjścia systemu y(t) otrzymanego jako odpowiedź na sterowanie u(t), do transformaty Laplace’a tego sterowania U(s) - przy zerowych warunkach początkowych",
+    ]
+  },
+  {
+    "question": "Transmitancja widmowa 𝑮(𝒋𝝎) obiektu regulacji może być wyznaczona analitycznie następująco:",
+    "options": [
+      "G(jw) = G(s), gdzie s=inf",
+      "G(jw) = G(s), gdzie s=0",
+      "G(jw) = G(s), gdzie s=jw",
+      "G(jw) = G(s), gdzie s=w",
+    ],
+    "answer": [
+      "G(jw) = G(s), gdzie s=jw",
+    ]
+  },
+  {
+    "question": "Które ze sformułowań dotyczących wzmocnienia proporcjonalnego 𝒌𝒓 regulatora PID są prawdziwe?",
+    "options": [
+      "zmniejszenie uchybu regulacji",
+      "niebezpieczeństwo utraty stabilności przy zbyt dużym wzmocnieniu",
+      "zwiększenie uchybu regulacji",
+      "pogorszenie dynamiki układu regulacji",
+    ],
+    "answer": [
+      "zmniejszenie uchybu regulacji",
+      "niebezpieczeństwo utraty stabilności przy zbyt dużym wzmocnieniu",
+    ]
+  },
+  {
+    "question": "Które ze sformułowań dotyczących części całkującej (I) regulatora PID są prawdziwe?",
+    "options": [
+      "część całkująca poprawia stabilność układu regulacji",
+      "część całkująca ma silne działanie dla sygnałów o dużej częstotliwości",
+      "część całkująca przyspiesza działanie układu regulacji",
+      "żadne z powyższych stwierdzeń nie jest prawdą",
+    ],
+    "answer": [
+      "żadne z powyższych stwierdzeń nie jest prawdą"
+    ]
+  },
+  {
+    "question": "Które ze sformułowań dotyczących części różniczkującej (D) regulatora PID są prawdziwe?",
+    "options": [
+      "część różniczkująca pozwala na eliminację uchybu ustalonego w układzie regulacji",
+      "część różniczkująca poprawia stabilność układu regulacji",
+      "część różniczkująca przyspiesza działanie układu regulacji",
+      "żadne z powyższych stwierdzeń nie jest prawdą",
+    ],
+    "answer": [
+      "część różniczkująca poprawia stabilność układu regulacji",
+      "część różniczkująca przyspiesza działanie układu regulacji"
+    ]
+  },
+  {
+    "question": "Które ze sformułowań dotyczących części różniczkującej (D) regulatora PID są prawdziwe?",
+    "options": [
+      "część różniczkująca pozwala na eliminację uchybu ustalonego w układzie regulacji",
+      "działanie części różniczkującej widoczne w stanie ustalonym",
+      "część różniczkująca zwiększa zapasy stabilności w układzie regulacji",
+      "część różniczkująca wzmacnia szumy i zakłócenia w układzie regulacji",
+    ],
+    "answer": [
+      "część różniczkująca zwiększa zapasy stabilności w układzie regulacji",
+      "część różniczkująca wzmacnia szumy i zakłócenia w układzie regulacji"
+    ]
+  },
+  {
+    "question": "Dane są mianowniki transmitancji obiektów regulacji M(s). Które z tych obiektów są stabilne asymptotycznie?",
+    "options": [
+      "𝑀(𝑠) = 3𝑠^2 + 5s",
+      "𝑀(𝑠) = 3𝑠^2 + 𝑠 + 2",
+      "𝑀(𝑠) = 𝑠^2 + 2𝑠 + 10",
+      "𝑀(𝑠) = 3𝑠^2 + 5𝑠 − 2",
+    ],
+    "answer": [
+      "𝑀(𝑠) = 3𝑠^2 + 𝑠 + 2",
+      "𝑀(𝑠) = 𝑠^2 + 2𝑠 + 10"
+    ]
+  },
+  {
+    "question": "Dane są wielomiany charakterystyczne systemów dynamicznych M(s). Które z tych systemów są niestabilne?",
+    "options": [
+      "𝑀(𝑠) = 𝑠^2 + 3𝑠 − 7",
+      "𝑀(𝑠) = 𝑠^2 + 2",
+      "𝑀(𝑠) = 3𝑠^2 + 2𝑠 + 1",
+      "𝑀(𝑠) = 𝑠^2 + 2𝑠 + 10",
+    ],
+    "answer": [
+      "𝑀(𝑠) = 𝑠^2 + 3𝑠 − 7"
+    ]
+  },
+  {
+    "question": "Dana jest charakterystyka amplitudowo-fazowa układu regulacji z otwartą pętlą sprzężenia zwrotnego. Dla fazy 𝝋(𝝎) = −𝟏𝟖𝟎 wartość części rzeczywistej transmitancji widmowej 𝑷(𝝎) = −𝟎.𝟕𝟓. Układ regulacji po zamknięciu pętli sprzężenia zwrotnego będzie:",
+    "options": [
+      "niestabilny",
+      "stabilny",
+      "na granicy stabilności",
+      "nie da się nic powiedzieć o stabilności układu zamkniętego",
+    ],
+    "answer": [
+      "stabilny"
+    ]
+  },
+  {
+    "question": "Zapas stabilności (wyznaczany na charakterystyce Bodego) dla układu regulacji jest równy 10 dB. Oznacza to, że:",
+    "options": [
+      "zamknięty układ regulacji jest stabilny",
+      "zamknięty układ regulacji znajduje się na granicy stabilności",
+      "nie można nic powiedzieć na temat stabilności układu",
+      "zamknięty układ regulacji jest niestabilny",
+    ],
+    "answer": [
+      "zamknięty układ regulacji jest stabilny"
+    ]
+  },
+  {
+    "question": "Dany jest zamknięty układ regulacji złożony z obiektu inercyjnego II rzędu z opóźnieniem i regulatora liniowego. Który z poniższych regulatorów zapewni uchyb ustalony w układzie równy zero? ",
+    "options": [
+      "PD",
+      "P",
+      "PI",
+      "PID",
+    ],
+    "answer": [
+      "PI",
+      "PID"
+    ]
+  },
+  {
+    "question": "Czas regulacji w zamkniętym układzie regulacji może być określony na podstawie:",
+    "options": [
+      "przebiegu czasowego uchybu regulacji w tym układzie zamkniętym",
+      "przebiegu odpowiedzi skokowej obiektu regulacji",
+      "przebiegu odpowiedzi skokowej regulatora",
+      "przebiegu czasowego wielkości regulowanej w tym układzie zamkniętym",
+    ],
+    "answer": [
+      "przebiegu czasowego uchybu regulacji w tym układzie zamkniętym",
+      "przebiegu czasowego wielkości regulowanej w tym układzie zamkniętym"
+    ]
+  },
+  {
+    "question": "Dany jest całkowy wskaźnik jakości regulacji mający postać: 𝑰𝟑 = ∫𝛆^2(𝒕)𝒅𝒕, gdzie 𝜺(𝒕) oznacza uchyb regulacji. Zaznaczyć, który z regulatorów zapewni skończoną wartość tego wskaźnika jakości?",
+    "options": [
+      "P",
+      "PI",
+      "PD",
+      "PID",
+    ],
+    "answer": [
+      "PI",
+      "PID"
+    ]
+  },
+  {
+    "question": "Dany jest całkowy wskaźnik jakości regulacji mający postać: 𝑰1 = ∫𝛆^2(𝒕)𝒅𝒕, gdzie 𝜺(𝒕) oznacza uchyb regulacji. Zaznaczyć, do oceny jakich przebiegów czasowych uchybu regulacji 𝜺(𝒕) może on być stosowany:",
+    "options": [
+      "wyłącznie oscylacyjnych nietłumionych",
+      "wszystkich",
+      "aperiodycznych",
+      "wyłącznie oscylacyjnych tłumionych",
+    ],
+    "answer": [
+      "aperiodycznych"
+    ]
+  },
+  {
+    "question": "Jakiego typu obiektem dynamicznym jest siłownik pneumatyczny membranowy, którego wejściem jest ciśnienie zasilające a wyjściem położenie organu ruchomego?",
+    "options": [
+      "Inercyjnym 2 rzędu",
+      "2 rzędu oscylacyjnym lub aperiodycznym w zależności od jego parametrów",
+      "Inercyjnym 1 rzędu",
+      "Całkującym",
+    ],
+    "answer": [
+      "Inercyjnym 2 rzędu",
+      "2 rzędu oscylacyjnym lub aperiodycznym w zależności od jego parametrów"
+    ]
+  },
+  {
+    "question": "Dana jest różniczkowalna funkcja f(t)dla której istnieje transformata Laplace’a. Oznaczmy tę transformatę jako F(s), a różną od zera wartość początkową funkcji przez f(0). Wtedy transformata Laplace’a pierwszej pochodnej tej funkcji jest równa",
+    "options": [
+      "sF(s)+sf(0)",
+      "sF(s)+f(0)",
+      "sF(s)-f(0)",
+      "sF(s)-sf(0)",
+    ],
+    "answer": [
+      "sF(s)-f(0)"
+    ]
+  },
+  {
+    "question": "Dana jest wypadkowa transmitancja obiektu równa: 𝑮(𝒔) = 𝑮𝟏(𝒔) + 𝑮𝟐(𝒔) − 𝑮𝟑(𝒔). W jaki sposób są połączone transmitancje składowe tej transmitancji?",
+    "options": [
+      "równolegle",
+      "nic nie można powiedzieć o ich połączeniu",
+      "szeregowo",
+      "z ujemnym sprzężeniem zwrotnym",
+    ],
+    "answer": [
+      "równolegle"
+    ]
+  },
+  {
+    "question": "Dana jest doświadczalna odpowiedź skokowa obiektu: Co można stwierdzić na temat dynamiki tego obiektu?",
+    "options": [
+      "Można podać rząd i dokładny rozkład biegunów transmitancji",
+      "Na temat dynamiki obiektu nic nie da się stwierdzić na podstawie tej charakterystyki skokowej",
+      "Obiekt można opisać transmitancją zastępczą 1 lub 2 rzędu z opóźnieniem",
+      "Obiekt jest statyczny",
+    ],
+    "answer": [
+      "Obiekt można opisać transmitancją zastępczą 1 lub 2 rzędu z opóźnieniem",
+      "Obiekt jest statyczny",
+    ],
+    image: "img/pa1.png" 
+  },
+  {
+    "question": "Zaznacz prawdziwe sformułowania dotyczące regulatora PID",
+    "options": [
+      "Całkowanie w regulatorze zmniejsza zapasy stabilności w układzie",
+      "Część całkująca pozwala na eliminację uchybu ustalonego w układzie regulacji",
+      "Zwiększenie wzmocnienia statycznego zwiększa zapasy stabilności w układzie regulacji",
+      "Część różniczkująca zwiększa zapasy stabilności w układzie regulacji",
+    ],
+    "answer": [
+      "Całkowanie w regulatorze zmniejsza zapasy stabilności w układzie",
+      "Część całkująca pozwala na eliminację uchybu ustalonego w układzie regulacji",
+      "Część różniczkująca zwiększa zapasy stabilności w układzie regulacji"
+    ]
+  },
+  {
+    "question": "Dany jest system dyskretny opisany transmitancją operatorową: Zaznacz dla jakich wartości parametru a system jest asymptotycznie stabilny.",
+    "options": [
+      "𝑎 = −0.99",
+      "𝑎 = 1.00",
+      "𝑎 = −1.00",
+      "𝑎 = 0.10",
+    ],
+    "answer": [
+      "𝑎 = −0.99",
+      "𝑎 = 0.10",
+    ],
+    image: "img/pa2.png" 
+  },
+];
